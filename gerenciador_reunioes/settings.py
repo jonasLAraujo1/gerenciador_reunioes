4e9da419019324dbe5e7719a5bc5463ce18109b0
Jonas Lima <jonasaraujo2137@gmail.com>
2019-11-13 10:14:20 -0300

    teste com conta diferente teste 5

--- gerenciador_reunioes/settings.py
+++ gerenciador_reunioes/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.conf.global_settings import DATE_INPUT_FORMATS
 
@@ -128,4 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+django_heroku.settings(locals())
