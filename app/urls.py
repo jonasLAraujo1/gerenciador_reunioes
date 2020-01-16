6ebb77874b79ffd189120704c0a5775b6206bd89
Jonas Lima <jonasaraujo2137@gmail.com>
2020-01-16 14:28:21 -0300

    Página 404 personalizada

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
-
+from django.conf.urls import url
 from django.urls import path
+
+from django.conf import settings
+from django.template.response import TemplateResponse
 
 
 from .views.acoes_views import *
@@ -7,8 +10,8 @@ from .views.reunioes_views import *
 from .views.usuario_views import *
 
 
-
-urlpatterns = [
+if settings.DEBUG:
+    urlpatterns = [
     path('agendar/',agendar_reuniao,name="agendar_reuniao"),
     path('tipo/',agendar_tipo,name="agendar_tipo"),
     path('inicio/',calendario,name="calendario"),
@@ -21,6 +24,8 @@ urlpatterns = [
     path('login/', logar_usuario, name="logar_usuario"),
     path('logout/', deslogar_usuario, name="deslogar_usuario"),
     path('ata/<int:id>', ata, name="ata"),
+    url(r'^', TemplateResponse, {'template': '404.html'}),
     ]
+
 # handler404 = 'app.views.acoes_views.erro_404'
 # handler500 = 'app.views.acoes_views.erro_500'
